3844880b4464da89ce2c24436328cdbcc51d5db9
Giovanna Fernandes <pfernandesgiovanna07@gmail.com>
2026-02-27 15:27:43 -0300

    Projeto BilheteUnico completo

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -8,23 +8,41 @@ public class SistemaPrincipal {
    public static void main(String[] args) {
 	   
        // Instância 1
-       BilheteUnico bilhete1 = new BilheteUnico();
-       bilhete1.tipo = "estudante";
+       BilheteUnico bilhete1 = new BilheteUnico(null, 0, null);
+       bilhete1.tipo = "Estudante";
        bilhete1.saldo = 20.00;
        bilhete1.dataValidade = LocalDate.of(2026, 12, 31);
        
        // Instância 2
-       BilheteUnico bilhete2 = new BilheteUnico();
-       bilhete2.tipo = "comum";
-       bilhete2.saldo = 5.00;
+       BilheteUnico bilhete2 = new BilheteUnico(null, 0, null);
+       bilhete2.tipo = "Comum";
+       bilhete2.saldo = 4.00;
        bilhete2.dataValidade = LocalDate.of(2025, 5, 10);
+
+       boolean entrada1 = bilhete1.validarEntrada(5.0);
+       boolean entrada2 = bilhete2.validarEntrada(5.0);
        
-       System.out.println("Bilhete 1 tipo: " + bilhete1.tipo
-    		   + ", saldo: " + bilhete1.saldo
-						 + "e data de validade: " + bilhete1.dataValidade);
-       System.out.println("Bilhete 2 tipo: " + bilhete2.tipo
-       		+ ", saldo: " + bilhete2.saldo
-						 + "e data de validade: " + bilhete2.dataValidade);
+       System.out.println("\n===== BILHETE 1 =====");
+
+       if (bilhete1.estaVencido()) {
+         System.out.println ("Bilhete vencido.");
+       }
+
+       System.out.println ("Tipo: " + bilhete1.tipo);
+       System.out.println ("Saldo: R$ " + String.format("%.2f", bilhete1.saldo));
+       System.out.println ("Data de validade: " + bilhete1.dataValidade);
+       System.out.println ("Entrada liberada? " + (entrada1 ? "SIM" : "NÃO"));
+
+       System.out.println ("\n===== BILHETE 2 =====");
+
+       if (bilhete2.estaVencido()) {
+         System.out.println ("Bilhete vencido.");
+       }
+
+       System.out.println ("Tipo: " + bilhete2.tipo);
+       System.out.println ("Saldo: R$ " +  String.format("%.2f", bilhete2.saldo));
+       System.out.println ("Data de validade: " + bilhete2.dataValidade);
+       System.out.println ("Entrada liberada? " + (entrada2 ? "SIM" : "NÃO"));
        
    }
 }
--- src/br/com/fiapride/model/BilheteUnico.java
+++ src/br/com/fiapride/model/BilheteUnico.java
@@ -3,11 +3,38 @@ package br.com.fiapride.model;
 import java.time.LocalDate;
 
 public class BilheteUnico {
-	
 
-    // características 
     public String tipo;
     public double saldo;
     public LocalDate dataValidade;
-    
-}
+
+    public BilheteUnico(String tipo, double saldo, LocalDate dataValidade) {
+        this.tipo = tipo;
+        this.saldo = saldo;
+        this.dataValidade = dataValidade;
+    }
+
+    public boolean estaVencido() {
+        return dataValidade.isBefore(LocalDate.now());
+    }
+
+    public boolean pagarPassagem(double valorTarifa) {
+
+        if (estaVencido()) {
+            return false;
+        }
+        
+        if (tipo.equalsIgnoreCase("Estudante")) {
+            valorTarifa = valorTarifa * 0.5;
+        }
+        
+        if (saldo < valorTarifa) {
+            return false;
+        }
+        saldo -= valorTarifa;
+        return true;
+    }
+    public boolean validarEntrada(double valorTarifa) {
+        return pagarPassagem(valorTarifa);
+    }
+ }
